1721cf3dc153d0eacb567a95e168620858e78bc8
mehrdadanvar <mehrdad.anvar@gmail.com>
2021-05-17 18:25:19 +0430

    some changes

--- mehrdad.js
+++ mehrdad.js
@@ -17,5 +17,23 @@ const newparagrap = document.createElement('p');
 newparagrap.textContent = "hello new content is here";
 body.append(newparagrap);
 
-const buttons = document.querySelectorAll('button');
+//creating a table from the dom
+// const previousTable = document.querySelector('table'); //does not work
+// const previousTable = document.getElementById('1');
+// const newTable = document.createElement('table');
+// body.append(newTable);
+// console.log("successfuuly inserted a table");
 
+//cleaning data from the table
+const tableData = document.querySelector('table.kook-holder');
+const analyzing = tableData.rows[1].innerText;
+console.log(analyzing[0]);
+const tobeadded = document.createElement('ul');
+body.append(tobeadded);
+
+// body.append(mehrdad);
+
+
+
+// const godknowswhat = tableData[0].innerText;
+// console.log(godknowswhat);
